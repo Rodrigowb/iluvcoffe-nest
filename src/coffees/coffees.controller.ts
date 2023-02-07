73218980c23a46959e1d0e63cf8213ee01e14c7f
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2023-02-07 07:31:36 -0300

    learning about how to return route parameters with @Param decorators and how to make post requests and receive the payload using the @Body decorator

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get('flavors')
   findAll() {
     return 'This action returns all coffees';
+  }
+
+  // Return route parameters
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
+  }
+
+  // Request body/ payload
+  @Post()
+  create(@Body() body) {
+    return body;
   }
 }
