e6819b3fac898d420a3d45f70efc7d8c7c458631
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2023-02-07 07:55:53 -0300

    learning about update requests (put and patch) and delete requests. For the patch request, we use the @Param to define what to update and the @Body to define the new value. To delete, we use @Delete with @Param to define what to delete

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -14,8 +14,23 @@ export class CoffeesController {
   }
 
   // Request body/ payload
-  @Post()
-  create(@Body() body) {
+  @Post()  
+  create(@Body('name') body) {
     return body;
   }
+
+  // Update patch request
+  @Patch(':id')
+  update(@Param('id') id: String, @Body() body) {
+    return `This action updates #${id} coffee`
+  }
+
+  // Delete requests
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`
+  }
+
+
 }
+ 
