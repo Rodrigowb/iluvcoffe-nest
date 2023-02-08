1f98102467421acab7858303b332e5201f99820e
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2023-02-08 07:19:10 -0300

    replacing the controller with the service methods to segregate it from the business logic and interaction with the data source

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,41 +1,47 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
   // Inject services in the controller
   constructor(private readonly coffeesService: CoffeesService) {
-    
+
   }
   @Get('flavors')
     // Use query to paginate the response
   findAll(@Query() paginationQuery) {
     // Object destructuring to come in every request
-    const { limit, offset } = paginationQuery;
-    return `This action returns all coffees. Limit: ${limit}, offset: ${offset}`;
+    // const { limit, offset } = paginationQuery;
+    // return `This action returns all coffees. Limit: ${limit}, offset: ${offset}`;
+    return this.coffeesService.findAll();
   }
 
   // Return route parameters
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This action returns #${id} coffee`;
+    // return `This action returns #${id} coffee`;
+    return this.coffeesService.findOne(id);
   }
 
   // Request body/ payload
   @Post()  
   create(@Body('name') body) {
-    return body;
+    // return body;
+    return this.coffeesService.create(body);
   }
 
   // Update patch request
   @Patch(':id')
-  update(@Param('id') id: String, @Body() body) {
-    return `This action updates #${id} coffee`
+  update(@Param('id') id: string, @Body() body) {
+    // return `This action updates #${id} coffee`
+    return this.coffeesService.update(id, body);
   }
 
   // Delete requests
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This action removes #${id} coffee`
+    // return `This action removes #${id} coffee`
+    return this.coffeesService.remove(id);
   }
 
 
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,6 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.intity';
 
 @Injectable()
 export class CoffeesService {
-  private coffees = [];
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Geisha',
+      brand: 'Coffe Mandalorian',
+      flavors: ['chocolate', 'vanilla'],
+    }
+  ];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    return this.coffees.find(item => item.id === +id);
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      // Upadate the existing entity
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex(item => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
 }
