401c7e6d4998d87fcc976c483a0cf8c48f34f0e0
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2023-02-08 12:50:30 -0300

    creating module to encapsulate close related capabilities and removing the controllers and providers from the app module to avoid duplication

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { CoffeesController } from './coffees/coffees.controller';
 import { MugsController } from './mugs/mugs.controller';
 import { CoffeesService } from './coffees/coffees.service';
+import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CoffeesController, MugsController],
-  providers: [AppService, CoffeesService],
+  imports: [CoffeesModule],
+  controllers: [AppController, MugsController],
+  providers: [AppService],
 })
 export class AppModule {}
