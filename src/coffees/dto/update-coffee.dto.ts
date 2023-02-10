12ebcb27eb87f605384817735a19dca503b37b45
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2023-02-10 10:22:56 -0300

    extending the updateCoffeeDto from createCoffeeDto to avoid code duplicity

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,5 +1,5 @@
-export class UpdateCoffeeDto {
-  readonly name?: string;
-  readonly brand?: string;
-  readonly flavors?: string[];
-}
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCoffeeDto } from './crate-coffee.dto'
+
+// Extands the CreateCoffeeDto to avoid code duplicity
+export class UpdateCoffeeDto extends PartialType{CreateCoffeeDto} {}
