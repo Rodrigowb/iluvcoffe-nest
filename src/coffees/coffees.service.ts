15b646256dac131156225cd1e7cce885d212123d
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2023-02-10 10:45:13 -0300

    add transform to validationpipe, transforming objects to instances of its original class and transform primitiv datatypes

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -27,7 +27,6 @@ export class CoffeesService {
 
   create(createCoffeeDto: any) {
     this.coffees.push(createCoffeeDto);
-    return createCoffeeDto;
   }
 
   update(id: string, updateCoffeeDto: any) {
