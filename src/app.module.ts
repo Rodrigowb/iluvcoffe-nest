d417a96319c462c8b11949cf209a4d279e7cb6ed
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2023-02-14 07:20:46 -0300

    remove unecessary imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MugsController } from './mugs/mugs.controller';
 import { CoffeesModule } from './coffees/coffees.module';
-import { MugsService } from './mugs/mugs.service';
 import { MugsModule } from './mugs/mugs.module';
 
 @Module({
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
+  // Make the application use validation pipes
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: true
+  }));
   await app.listen(3000);
 }
 bootstrap();
