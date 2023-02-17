170611f335eeabb2baed3fe4117db1104d54e9b4
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2023-02-17 09:13:45 -0300

    testing and remoing unecessary comments. Worked very fine

--- src/mugs/mugs.controller.ts
+++ src/mugs/mugs.controller.ts
@@ -12,35 +12,28 @@ export class MugsController {
 
   // Endpoints go here
   @Get()
-  findAll(@Query() paginationQuery) {
-    // Destructure object
-    // const { limit, offset } = paginationQuery
-    // return `Endpoint get all mugs with pagination. Limit: ${limit} Offset: ${offset}`
+  findAll() {
     return this.mugsService.findAll()
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    // return `Endpoint returns a mug by it's id #${id}`
     return this.mugsService.findOne(id)
 
   }
 
   @Post()
   create(@Body() createMugsDto: CreateMugsDto) {
-    // return `Endpoint creats a mug. Name of mug created: ${body}`
     return this.mugsService.create(createMugsDto)
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateMugsDto: UpdateMugsDto) {
-    // return `Endpoint updates a mug. Updating id#${id} to new body named #${body}`
     return this.mugsService.update(id, updateMugsDto)
   }
 
   @Delete(':id')
   delete(@Param('id') id: string) {
-    // return `Endpoint delets a mug. Deleting mug id#${id}`
     return this.mugsService.remove(id)
   }
 
