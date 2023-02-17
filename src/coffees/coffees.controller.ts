84ab8dfaf6275df149717fcfd757641b0838d9c0
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2023-02-17 07:31:22 -0300

    removing unecessary comments

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -11,38 +11,31 @@ export class CoffeesController {
   }
   @Get('flavors')
     // Use query to paginate the response
-  findAll(@Query() paginationQuery) {
-    // Object destructuring to come in every request
-    // const { limit, offset } = paginationQuery;
-    // return `This action returns all coffees. Limit: ${limit}, offset: ${offset}`;
+  findAll() {
     return this.coffeesService.findAll();
   }
 
   // Return route parameters
   @Get(':id')
   findOne(@Param('id') id: string) {
-    // return `This action returns #${id} coffee`;
     return this.coffeesService.findOne(id);
   }
 
   // Request body/ payload
   @Post()  
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
-    // return body;
     return this.coffeesService.create(createCoffeeDto);
   }
 
   // Update patch request
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
-    // return `This action updates #${id} coffee`
     return this.coffeesService.update(id, updateCoffeeDto);
   }
 
   // Delete requests
   @Delete(':id')
   remove(@Param('id') id: string) {
-    // return `This action removes #${id} coffee`
     return this.coffeesService.remove(id);
   }
 
